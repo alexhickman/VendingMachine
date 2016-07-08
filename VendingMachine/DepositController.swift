48db1cc5f8347132838499de5f84b77ece5b7ba0
alexhickman <alex.hickman3@gmail.com>
2016-07-08 15:11:13 -0700

    Initial Commit

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,20 @@
+//
+//  DepositController.swift
+//  VendingMachine
+//
+//  Created by Hickman on 6/30/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func dismiss() {
+        dismissViewControllerAnimated(true, completion: nil)    
+    }
+}
